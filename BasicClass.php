ecc26063df53e93c1dd642b4b0b12bea29fb90da
Fernando Palomo Garcia <fernando@bluejay.es>
2013-06-18 10:41:04 +0200

    tests ok

---
+++ BasicClass.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: fernando
+ * Date: 18/06/13
+ * Time: 10:40
+ * To change this template use File | Settings | File Templates.
+ */
+
+class BasicClass {
+
+
+    public function getTrue(){
+        return true;
+    }
+
+}
